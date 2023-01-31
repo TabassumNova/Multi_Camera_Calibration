1a7cfdcc664435f3457f6de2936ae390f57ce689
tabassumnova1 <tabassumnova1@gmail.com>
2023-01-31 13:01:38 +0100

    camera_automation_node_modified

--- camera_automation/src/aravis_image_acquisition.py
+++ camera_automation/src/aravis_image_acquisition.py
@@ -5,22 +5,16 @@ from collections import namedtuple
 from .aravis import Camera
 import cv2
 
-def main():
-    if len(sys.argv) > 1:
-        path = sys.argv[1]
-    else:
-        path = "frame.png"
-    #cam = aravis.Camera("AT-Automation Technology GmbH-20805103")
-    # cam = Aravis.Camera('The Imaging Source Europe GmbH-42120643')
-    cam = Camera('The Imaging Source Europe GmbH-11120241')
+def arv_image_acquisition(cam_model, exposure_time, path):
+    cam = Camera(cam_model)
     cam.set_feature("Width", 5472)
     cam.set_feature("Height", 3648)
     cam.set_frame_rate(2)
-    cam.set_exposure_time(2000000)
+    cam.set_exposure_time(exposure_time)
     #cam.start_acquisition_trigger()
     cam.start_acquisition_continuous()
     try:
-        path = '/home/raptor/tx60_moveit/src/tx60l_moveit_config/python_program/image/arv_im.png'
+        
         #cam.trigger()
         frame = cam.pop_frame()
         print("Saving image to ", path)
