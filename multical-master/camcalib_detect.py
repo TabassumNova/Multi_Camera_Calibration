955d25b94b35536c1d3846c2b288172db2f18918
TabassumNova <77242943+TabassumNova@users.noreply.github.com>
2023-05-24 09:48:10 +0200

    multical_master_added

--- multical-master/camcalib_detect.py
+++ multical-master/camcalib_detect.py
@@ -2,8 +2,10 @@ import multical.app.boards as boards
 
 if __name__ == '__main__':
 
-    board_yaml = "D:\MY_DRIVE_N\Masters_thesis\mymy\camnodes_pkg\camnodes_pkg\multical-master\detect_test/t2.yaml"
-    detect_img = "D:\MY_DRIVE_N\Masters_thesis\mymy\camnodes_pkg\camnodes_pkg\multical-master\detect_test/p2.bmp"
-    b = boards.Boards(boards=board_yaml, detect=detect_img)
+    board_yaml = "D:/MY_DRIVE_N/Masters_thesis/Dataset/Latest Pattern/board_images/boards.yaml"
+    detect_img = "D:/MY_DRIVE_N/Masters_thesis/Dataset/Latest Pattern/board_images/b108.png"
+    b = boards.Boards(boards=board_yaml, detect=detect_img, pixels_mm=10)
+    # b = boards.Boards(boards=board_yaml, detect=detect_img)
 
     b.execute()
+
